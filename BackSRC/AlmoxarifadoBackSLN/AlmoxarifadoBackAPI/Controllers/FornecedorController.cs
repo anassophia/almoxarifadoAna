23be197acb47e1e20583866c4a49a7fc8fc08f36
Enzzo Samuel <144052012+anassophia@users.noreply.github.com>
2025-03-10 09:30:49 -0300

    fornecedor

--- BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/Controllers/FornecedorController.cs
+++ BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/Controllers/FornecedorController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AlmoxarifadoBackAPI.DTO;
+using AlmoxarifadoBackAPI.Models;
+using AlmoxarifadoBackAPI.Repositorio;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AlmoxarifadoBackAPI.Controllers
@@ -7,5 +10,41 @@ namespace AlmoxarifadoBackAPI.Controllers
     [ApiController]
     public class FornecedorController : ControllerBase
     {
+        private readonly IFornecedorRepositorio _db;
+        public FornecedorController(IFornecedorRepositorio db)
+        {
+            _db = db;
+
+        }
+
+        [HttpGet("/lista")]
+        public IActionResult listaFornecedor()
+        {
+            return Ok(_db.GetAll());
+        }
+
+        [HttpPost("/fornecedor")]
+        public IActionResult listaFornecedor(FornecedorDTO fornecedor)
+        {
+            return Ok(_db.GetAll().Where(x => x.Codigo == fornecedor.Codigo));
+        }
+
+        [HttpPost("/criarfornecedor")]
+        public IActionResult criarfornecedor(FornecedorCadastroDTO fornecedor)
+        {
+
+            var novoFornecedor = new Fornecedor()
+            {
+                Nome = fornecedor.Nome,
+                Telefone = fornecedor.Telefone,
+                Estado = fornecedor.Estado,
+                Cidade = fornecedor.Cidade,
+                CNPJ = fornecedor.CNPJ
+
+            };
+            //_categorias.Add(novaCategoria);
+            _db.Add(novoFornecedor);
+            return Ok("Cadastro com Sucesso");
+        }
     }
 }
