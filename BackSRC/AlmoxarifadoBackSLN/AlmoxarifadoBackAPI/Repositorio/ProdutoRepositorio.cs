e87dda99d2d96ec83b1a09844549970756b181cb
Enzzo Samuel <144052012+anassophia@users.noreply.github.com>
2025-03-10 09:14:53 -0300

    fork novo

--- BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/Repositorio/ProdutoRepositorio.cs
+++ BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/Repositorio/ProdutoRepositorio.cs
@@ -11,14 +11,14 @@ namespace AlmoxarifadoBackAPI.Repositorio
             _db = db;
         }
 
-        public void Add(Produto obj)
+        public void Add(Produto produto)
         {
-            
-            _db.Produto.Add(obj);
+
+            _db.Produto.Add(produto);
             _db.SaveChanges();
         }
 
-        public List<Produto> GetAll() { 
+        public List<Produto> GetAll() {
           return _db.Produto.ToList();
         }
 
