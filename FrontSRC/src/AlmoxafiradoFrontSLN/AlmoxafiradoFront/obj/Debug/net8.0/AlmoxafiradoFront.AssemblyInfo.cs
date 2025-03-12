0252275d8cf7402d6d516618513295eaeda35dfd
Guilherme Viana Santana <144052012+anassophia@users.noreply.github.com>
2025-03-12 15:19:20 -0300

    add coisinhas

--- FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
+++ FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23be197acb47e1e20583866c4a49a7fc8fc08f36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7336a485dac325000421d58aa19646fde04621f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
