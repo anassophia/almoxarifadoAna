4a61b14d036b7f4681ae2d0400e210e9b0132050
Enzzo Samuel <144052012+anassophia@users.noreply.github.com>
2025-03-17 09:20:47 -0300

    a

--- FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
+++ FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b18a3f7460e2d2a7cd1a3846dac3076564b027d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e13c9cdbae3f771b3c3241f77a7c7375d7e9c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
